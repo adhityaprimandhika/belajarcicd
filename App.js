eaeec9ca8fc49fe7ee0c253e2cb1f36077c22ec2
Adhitya Primandhika <adhityaprimandhika@gmail.com>
2024-03-06 16:29:33 +0700

    add input text

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { useEffect } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+} from "react-native";
+import { useEffect, useState } from "react";
 import {
   collection,
   addDoc,
@@ -8,6 +16,7 @@ import {
   doc,
   setDoc,
   deleteDoc,
+  getDocs,
 } from "firebase/firestore";
 import db from "./firebaseConfig";
 
@@ -45,81 +54,89 @@ export default function App() {
   //   // updateCity();
   // }, []);
 
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [loadingData, setLoadingData] = useState(false);
+  const [inputData, setInputData] = useState({
+    name: "",
+    country: "",
+  });
+
+  const fetchData = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "cities"));
+      const documents = [];
+      querySnapshot.forEach((doc) => {
+        documents.push({ id: doc.id, ...doc.data() });
+      });
+
+      setData(documents);
+
+      console.log("Update JSON Data : ");
+      console.log(documents);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
   const addCity = async () => {
     try {
-      const docRef = await addDoc(collection(db, "cities"), {
-        name: "Tokyo",
-        country: "Japan",
-      });
-      console.log("Document written with ID: ", docRef.id);
+      setLoading(true);
+      const docRef = await addDoc(collection(db, "cities"), inputData);
+      console.log("Document added with ID: ", docRef.id);
+      fetchData();
+      setLoading(false);
     } catch (e) {
+      setLoading(false);
       console.error("Error adding document: ", e);
     }
   };
 
-  const getCity = async () => {
-    const docRef = doc(db, "cities", "90jTeBwc5mTvXXzwDEvt");
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      console.log("No such a document");
-    }
-  };
-
-  const updateCity = async () => {
-    const docRef = doc(db, "cities", "90jTeBwc5mTvXXzwDEvt");
-    await setDoc(docRef, {
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA",
-    });
-
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      console.log("No such a document");
-    }
-  };
-
-  const deleteCity = async () => {
-    const docRef = doc(db, "cities", "MUwFG3rt0j4TYgpBw7ug");
-    const docSnap = await getDoc(docRef);
+  const deleteCity = async (id) => {
+    setLoadingData(true);
+    const docRef = doc(db, "cities", id);
     await deleteDoc(docRef);
-    console.log("Document deleted:", docSnap.data());
+    fetchData();
+    setLoadingData(false);
+    console.log("Document deleted");
   };
+
+  useEffect(() => {
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Halo nama saya Adhit!</Text>
-      <Button
-        title="Add Data"
-        onPress={() => {
-          addCity();
-        }}
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        placeholder="Name"
+        onChangeText={(text) => setInputData({ ...inputData, name: text })}
+        value={inputData.name}
       />
-      <Button
-        title="Get Data"
-        onPress={() => {
-          getCity();
-        }}
+      <TextInput
+        placeholder="Country"
+        onChangeText={(text) => setInputData({ ...inputData, country: text })}
+        value={inputData.country}
       />
-      <Button
-        title="Update Data"
-        onPress={() => {
-          updateCity();
-        }}
-      />
-      <Button
-        title="Delete Data"
-        color="red"
-        onPress={() => {
-          deleteCity();
-        }}
-      />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Button title="Add Data" onPress={addCity} />
+      )}
+      <Text>List Data</Text>
+      {loadingData ? (
+        <ActivityIndicator />
+      ) : (
+        data.map((item) => (
+          <View key={item.id}>
+            <Text style={{ textAlign: "center" }}>Name: {item.name}</Text>
+            <Text style={{ textAlign: "center" }}>Country: {item.country}</Text>
+            <Button title="Delete" onPress={() => deleteCity(item.id)} />
+          </View>
+        ))
+      )}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
